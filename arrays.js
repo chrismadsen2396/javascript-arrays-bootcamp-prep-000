888adcfbab1fff9f70d79b3567e7971652db67c9
chrismadsen2396@gmail.com <chrismadsen2396@gmail.com>
2018-02-28 05:05:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snicker', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, newElement) {
-  return array [newElement, ...]
+  return array [newElement, ...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, newElement) {
